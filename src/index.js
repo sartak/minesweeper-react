d79ced62333878c302be8ae891daebc506c71dcf
Shawn M Moore <code@sartak.org>
2018-01-30 01:48:57 -0500

    hot reloading
    https://daveceddia.com/hot-reloading-create-react-app/

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
 registerServiceWorker();
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
+
