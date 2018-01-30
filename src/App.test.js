fd7cbbd282b75ff137fd9d4c02164c61ef7758fd
Shawn M Moore <code@sartak.org>
2018-01-30 02:40:28 -0500

    Add hot-reloading reducers

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import rootStore from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootStore);
+let reducers = rootStore;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import rootStore from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import { AppContainer } from 'react-hot-loader';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootStore, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const render = Component => {
   ReactDOM.render(
-    <AppContainer>
-      <App />
-    </AppContainer>,
+    <Provider store={store}>
+      <AppContainer>
+        <App />
+      </AppContainer>
+    </Provider>,
     document.getElementById('root')
   );
 };
@@ -21,6 +28,11 @@ render(App);
 if (module.hot) {
   module.hot.accept('./App', () => {
     render(App);
+  });
+
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
   });
 }
 
