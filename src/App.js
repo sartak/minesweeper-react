700315e9ac9867b1e870fec69b79847b0430f24f
Shawn M Moore <code@sartak.org>
2018-01-31 13:03:43 -0500

    Minesweeper with redux!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,140 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+
+class Tile extends Component {
+    rightClick = (e) => {
+        e.preventDefault();
+        this.props.rightClick();
+    }
+
+    render() {
+        let className = "tile-new";
+        if (this.props.flagged) {
+            if (this.props.revealed && this.props.value !== 'mine') {
+                className = "tile-mine-x";
+            }
+            else {
+                className = "tile-flag";
+            }
+        }
+        else if (this.props.revealed) {
+            className = "tile-" + this.props.value;
+        }
+
+        return (
+            <span onClick={this.props.onClick} onContextMenu={this.rightClick} className={"tile " + className} />
+        );
+    }
+}
+
+class Board extends Component {
+    tileClick = (x, y) => () => {
+        this.props.tileClick(x, y);
+    }
+
+    tileRightClick = (x, y) => () => {
+        this.props.tileRightClick(x, y);
+    }
+
+    render() {
+        return (
+            <div className="Board">
+                { this.props.grid.map( (row, y) => (
+                    <div key={y} className="row">
+                        { row.map( (tile, x) => (
+                            <Tile key={x} onClick={this.tileClick(x, y)} rightClick={this.tileRightClick(x, y)} {...tile} />
+                        ))}
+                    </div>
+                )) }
+            </div>
+        );
+    }
+}
+
+const BoardState = connect(
+    state => ({
+        grid: state.grid
+    }),
+    dispatch => ({
+        tileClick:      (x, y) => dispatch({ type: 'TILE_CLICK', x, y, time: new Date() }),
+        tileRightClick: (x, y) => dispatch({ type: 'TILE_FLAG', x, y }),
+    })
+)(Board);
+
+class ClockyDisplay extends Component {
+    render() {
+        var digits = "000" + Math.max(0, this.props.value);
+        digits = digits.substr(digits.length - 3);
+
+        return (
+            <div className={"clocky-display " + this.props.name}>
+                {
+                    digits.split("").map( (value, index) => (
+                        <div className={"timer-digit timer-"+value} key={index} />
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+class Smiley extends Component {
+    render() {
+        return (
+            <div onClick={this.props.onClick} className={"face face-" + this.props.phase} />
+        );
+    }
+}
+
+class HUD extends Component {
+    render() {
+        return (
+            <div className="HUD">
+                <ClockyDisplay name="mines-left" value={this.props.minesLeft} />
+                <Smiley phase={this.props.phase} onClick={this.props.restartGame} />
+                <ClockyDisplay name="time-spent" value={this.props.timeSpent} />
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => this.props.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+}
+
+const HUDState = connect(
+    state => ({
+        minesLeft: state.minesLeft,
+        timeSpent: state.timeSpent,
+        phase: state.phase,
+    }),
+    dispatch => ({
+        tick: () => dispatch({ type : 'TICK' }),
+        restartGame: () => dispatch({ type : 'RESTART' }),
+    })
+)(HUD);
+
+class Minesweeper extends Component {
+    render() {
+        return (
+            <div className="Minesweeper">
+                <HUDState />
+                <BoardState />
+            </div>
+        );
+    }
+}
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Minesweeper />
             </div>
         );
     }
--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,220 @@
-const rootStore = (state = {}, action) => {
+import seedrandom from 'seedrandom';
+
+const initialTile = {
+    revealed: false,
+    flagged: false,
+    value: 0
+}
+
+const initialState = {
+    mines: 15,
+    minesLeft: 15,
+    phase: 'init',
+    timeSpent: 0,
+    grid: Array(10).fill(Array(10).fill(initialTile)),
+};
+
+const tileStore = (state = initialTile, action) => {
     switch (action.type) {
+        case 'TILE_CLICK':
+            return { ...state, revealed: true };
+        case 'TILE_FLAG':
+            return { ...state, flagged: !state.flagged };
+        case 'GENERATE_MINE':
+            return { ...state, value: 'mine' };
+        case 'WON':
+            return {
+                ...state,
+                flagged: state.value === 'mine',
+            };
+        case 'LOSS':
+        case 'LOSS-REASON':
+            if (state.value === 'mine' || state.flagged) {
+                return {
+                    ...state,
+                    revealed: true,
+                    value: action.type === 'LOSS-REASON' ? 'mine-red' : state.value,
+                };
+            }
+            else {
+                return state;
+            }
+        default:
+            return state;
+    }
+}
+
+const rowStore = (state = [], action) => {
+    switch (action.type) {
+        case 'TILE_CLICK':
+        case 'TILE_FLAG':
+        case 'GENERATE_MINE':
+        case 'LOSS-REASON':
+            return state.map((tile, x) => tileStore(tile, x === action.x ? action : {}));
+        default:
+            return state.map(tile => tileStore(tile, action));
+    }
+}
+
+const eachAdjacent = (grid, x, y, callback) => {
+    for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+            if (dx || dy) {
+                if (y+dy >= 0 && y+dy < grid.length &&
+                    x+dx >= 0 && x+dx < grid[y+dy].length) {
+                        let tile = grid[y+dy][x+dx];
+                        callback(tile, x+dx, y+dy);
+                }
+            }
+        }
+    }
+
+    return grid;
+};
+
+const eachTile = (grid, callback) => {
+    for (let y = 0; y < grid.length; ++y) {
+        for (let x = 0; x < grid[y].length; x++) {
+            let tile = grid[y][x];
+            callback(tile, x, y);
+        }
+    }
+};
+
+const scoreTiles = (state = { }) => {
+    let grid = state.grid;
+
+    return {
+        ...state,
+        grid: grid.map((row, y) => row.map((tile, x) => {
+            if (tile.value === 'mine') {
+                return tile;
+            }
+            else {
+                let value = 0;
+                eachAdjacent(grid, x, y, tile => tile.value === 'mine' && value++);
+
+                return {
+                    ...tile,
+                    value: value,
+                };
+            }
+        })),
+    };
+}
+
+const generateMines = (state = { }, action, minesLeft) => {
+    let grid = state.grid;
+
+    if (minesLeft === 0) {
+        return scoreTiles(state);
+    }
+
+    let mine_y;
+    let mine_x;
+
+    var rng = seedrandom(action.time.getTime())
+
+    while (1) {
+        mine_y = Math.floor(rng() * grid.length);
+        mine_x = Math.floor(rng() * grid[mine_y].length);
+
+        let tile = rowStore(grid[mine_y], {})[mine_x];
+
+        if (tile.value !== 'mine' && (mine_y !== action.y || mine_x !== action.x)) {
+            break;
+        }
+    }
+
+    let newGrid = grid.map((row, y) => rowStore(grid[y], y === mine_y ? { type: 'GENERATE_MINE', x: mine_x, y: mine_y } : {}));
+    return generateMines({ ...state, grid: newGrid }, action, minesLeft - 1);
+}
+
+const rootStore = (state = initialState, action) => {
+    switch (action.type) {
+        case 'TICK':
+            return {
+                ...state,
+                timeSpent: (state.timeSpent + (state.phase === 'play' ? 1 : 0)),
+            };
+
+        case 'TILE_FLAG': {
+            if (state.phase !== 'init' && state.phase !== 'play') {
+                return state;
+            }
+
+            let grid = state.grid;
+            let tile = grid[action.y][action.x];
+            if (tile.revealed) {
+                return state;
+            }
+
+            grid = grid.map((row, y) => rowStore(row, y === action.y ? action : {}));
+            let flags = 0;
+            eachTile(grid, tile => tile.flagged && flags++);
+
+            return {
+                ...state,
+                grid: grid,
+                minesLeft: state.mines - flags,
+            };
+        }
+
+        case 'TILE_CLICK': {
+            let grid = state.grid;
+
+            if (state.phase === 'init') {
+                grid = generateMines(state, action, state.mines).grid;
+            }
+            else if (state.phase !== 'play') {
+                return state;
+            }
+
+            let phase = 'play';
+            let tile = grid[action.y][action.x];
+
+            if (tile.revealed || tile.flagged) {
+                // nothing to do
+            }
+            else if (tile.value === 'mine') {
+                phase = 'loss';
+                grid = grid.map(row => rowStore(row, { ...action, type: 'LOSS' }));
+                grid = grid.map((row, y) => rowStore(row, y === action.y ? { ...action, type: 'LOSS-REASON' } : {}));
+            }
+            else {
+                grid = grid.map((row, y) => rowStore(row, y === action.y ? action : {}));
+                if (tile.value === 0) {
+                    eachAdjacent(grid, action.x, action.y, (tile, x, y) => {
+                        if (!tile.revealed) {
+                            grid = rootStore({...state, grid, phase}, {...action, x, y}).grid
+                        }
+                    });
+                }
+
+                let unrevealed = 0;
+                eachTile(grid, tile => !tile.revealed && unrevealed++);
+                if (unrevealed === state.mines) {
+                    phase = 'won';
+                    grid = grid.map(row => rowStore(row, { type: 'WON' }));
+                    return {
+                        ...state,
+                        grid: grid,
+                        phase: phase,
+                        minesLeft: 0
+                    }
+                }
+            }
+
+            return {
+                ...state,
+                grid: grid,
+                phase: phase
+            };
+        }
+
+        case 'RESTART':
+            return initialState;
+
         default:
             return state;
     }
